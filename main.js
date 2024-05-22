623eb324f8928cc800edbaa6ff1f4476effa60cc
shanmugam2005 <123760760+shanmugam2005@users.noreply.github.com>
2024-05-22 11:03:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings :["Frontend Developer","Artificial intelligent","Data Analyst"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelat:1000,
+    loop:true
+
+});
