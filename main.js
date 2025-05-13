dbe4edbd93b751fbf8c86c1e4c81a8cf4182b2f1
shanmugam2005 <123760760+shanmugam2005@users.noreply.github.com>
2025-05-13 13:57:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings :["Frontend Developer","Artificial intelligent","Data Analyst"],
+    strings :["FullStack Developer","Data Analyst"],
     typeSpeed:100,
     backSpeed:100,
     backDelat:1000,
